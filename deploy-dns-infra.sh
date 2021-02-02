7a9f8239ff7a6071341a7c94c018ef4bfa8c4ffe
vplauzon <vplauzon@vplauzon.com>
2021-02-01 19:15:41 -0500

    Add verification id for gram-parser

--- deploy-dns-infra.sh
+++ deploy-dns-infra.sh
@@ -6,8 +6,10 @@
 ##  Parameters:
 ##
 ##  1- Name of resource group
+##  2- Value for gramParserVerificationId
 
 rg=$1
+gramParserVerificationId=$2
 
 echo "Resource group:  $rg"
 echo "Current directory:  $(pwd)"
@@ -17,4 +19,5 @@ echo "Deploying ARM template"
 
 az deployment group create -n "deploy-$(uuidgen)" -g $rg \
     --template-file dns.json \
-    --parameters @dns.parameters.json
+    --parameters @dns.parameters.json \
+    --parameters gramParserVerificationId=$gramParserVerificationId
